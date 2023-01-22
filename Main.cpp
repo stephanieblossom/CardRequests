4e21e73fe1e8313646006e28e86f44fa23a68d0e
Stephanie Blossom <73563399+stephanieblossom@users.noreply.github.com>
2023-01-22 00:44:16 -0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,149 +1,153 @@
-﻿// CPP code to illustrate Queue in 
-// Standard Template Library (STL)
+// CPP code for my card requests as VP of Finance
+// Utilizes queues, objects, and files
+// by: Stephanie Blossom (except for the sorting algorithm from stack overflow <3)
+
 #include <string>
+
 #include <fstream>
+
 #include <iostream>
+
 #include <queue>
+
+#include <float.h>
+
 using namespace std;
 
 class Officer {
-public:
-    string position;
-    double date;
-    double amount;
-    double time;
+  public: string position;
+  double date;
+  double amount;
+  string time;
 };
 
-// Print the queue
-void showq(queue<Officer> x){
-    queue<Officer> g = x;
-    while (!g.empty()) {
-        cout << '\t' << g.front().date;
-        g.pop();
-    }
+// Print queue method
+void showq(queue < Officer > x) {
+  queue < Officer > q = x;
+  while (!q.empty()) {
+    cout << '\n' << q.front().date;
+    cout << '\n' << q.front().position;
+    cout << '\n' << q.front().time;
+    cout << '\n' << q.front().amount;
     cout << '\n';
+    q.pop();
+  }
+  cout << '\n';
 }
 
-
-
-
-
-int minIndex(queue<Officer>& q, int sortedIndex)
-{
-    int min_index = -1;
-    int min_val = INT_MAX;
-    int n = q.size();
-    for (int i = 0; i < n; i++)
-    {
-        Officer curr = q.front();
-        q.pop();  // This is dequeue() in C++ STL
-
-        // we add the condition i <= sortedIndex
-        // because we don't want to traverse
-        // on the sorted part of the queue,
-        // which is the right part.
-        if (curr.date <= min_val && i <= sortedIndex)
-        {
-            min_index = i;
-            min_val = curr.date;
-        }
-        q.push(curr);
+int minIndex(queue < Officer > & q, int sortedIndex) {
+  int min_index = -1;
+  double min_val = DBL_MAX;
+  int n = q.size();
+  for (int i = 0; i < n; i++) {
+    Officer curr = q.front();
+    q.pop();
+    // we add the condition i <= sortedIndex
+    // because we don't want to traverse
+    // on the sorted part of the queue,
+    // which is the right part.
+    if (curr.date <= min_val && i <= sortedIndex) {
+      min_index = i;
+      min_val = curr.date;
     }
-    return min_index;
+    q.push(curr);
+  }
+  return min_index;
 }
 
 // Moves given minimum element to rear of 
 // queue
-void insertMinToRear(queue<Officer>& q, int min_index)
-{
-    Officer min_val;
-    int n = q.size();
-    for (int i = 0; i < n; i++)
-    {
-        Officer curr = q.front();
-        q.pop();
-        if (i != min_index)
-            q.push(curr);
-        else
-            min_val = curr;
-    }
-    q.push(min_val);
+void insertMinToRear(queue < Officer > & q, int min_index) {
+  Officer min_val;
+  int n = q.size();
+  for (int i = 0; i < n; i++) {
+    Officer curr = q.front();
+    q.pop();
+    if (i != min_index)
+      q.push(curr);
+    else
+      min_val = curr;
+  }
+  q.push(min_val);
 }
 
-void sortQueue(queue<Officer>& q)
-{
-    for (int i = 1; i <= q.size(); i++)
-    {
-        int min_index = minIndex(q, q.size() - i);
-        insertMinToRear(q, min_index);
-    }
+void sortQueue(queue < Officer > & q) {
+  for (int i = 1; i <= q.size(); i++) {
+    int min_index = minIndex(q, q.size() - i);
+    insertMinToRear(q, min_index);
+  }
 }
 
-int main(){
-    queue<Officer> requests;//initialize the queue and object attributes
-    double date;
-    string position{};
-    double amount;
-    double time;
+int main() {
+  queue < Officer > requests;
+  string position;
+  double date;
+  double amount;
+  string time;
 
-    // Create and open a text file
-    ofstream MyFile("queue.txt");
+  // Create and open a text file
+  ofstream MyFile("queue.txt");
 
-    // Write to the file
-    MyFile << "Files can be tricky, but it is fun enough!";
+  int flag = 0;
+  while (flag == 0) {
+    cout << "  REQUEST QUEUE\n\n";
+    cout << "Option 1: Print queue\n";
+    cout << "Option 2: Add to queue\n";
+    cout << "Option 3: Remove most recent\n";
+    cout << "Option 4: exit\n";
+    int option;
+    cin >> option;
 
-    // Close the file
-    MyFile.close();
+    if (option == 1) {
+      if (!requests.empty()) {
+        cout << "The queue of requests is : ";
+        showq(requests);
 
-
-    int flag = 0;
-    while (flag == 0) {
-        cout << "  REQUEST QUEUE　\n\n";
-        cout << "Option 1: Print queue\n";
-        cout << "Option 2: Add to queue\n";
-        cout << "Option 3: Remove most recent\n";
-        cout << "Option 4: exit?\n";
-        int option;
-        cin >> option;
-
-        if (option == 1) {
-            cout << "The queue of requests is : ";
-            showq(requests);
-
-            cout << "size: " << requests.size();
-            cout << "next person : " << requests.front().position;
-            cout << "last person) : " << requests.back().position;
-        }
-
-
-        if (option == 2) {
-            cout << "When does the officer need the card?\n";
-            cin >> date;
-            cout << "What is the officer's position?\n";
-            cin >> position;
-            cout << "What is the amount?\n";
-            cin >> amount;
-            cout << "Pickup time:\n";
-            cin >> time;
-
-
-            Officer obj1;
-            obj1.date = date;
-            obj1.amount = amount;
-            obj1.position = position;
-            obj1.time = time;
-
-            requests.push(obj1);
-            sortQueue(requests);
+        cout << "Next request is on: ";
+        cout << requests.front().date;
+      } else
+        cout << "The queue is empty.\n";
     }
-        if (option == 4) {
-            MyFile << requests; //fucking idk lol
 
-            cout << "\nrequests.pop() : ";
-            requests.pop();
-            showq(requests);
-            flag = 1;
-            return 0;
-        }
+    if (option == 2) {
+      cout << "When does the officer need the card?\n";
+      cin >> date;
+      cout << "What is the officer's position?\n";
+      cin >> position;
+      cout << "What is the amount?\n";
+      cin >> amount;
+      cout << "Pickup time:\n";
+      cin >> time;
+
+      Officer obj1;
+      obj1.date = date;
+      obj1.amount = amount;
+      obj1.position = position;
+      obj1.time = time;
+
+      requests.push(obj1);
+      sortQueue(requests);
     }
+    if (option == 4) {
+      while (!requests.empty()) {
+        MyFile << "Officer: ";
+        MyFile << requests.front().position;
+        MyFile << "\n";
+        MyFile << "Date needed: ";
+        MyFile << requests.front().date;
+        MyFile << "\n";
+        MyFile << "Time of pickup(if applicable): ";
+        MyFile << requests.front().time;
+        MyFile << "\n";
+         MyFile << "Amount: ";
+        MyFile << requests.front().time;
+        MyFile << "\n";
+        requests.pop();
+      }
+      MyFile.close();
+      flag = 1;
+    }
+
+  }
+  return 0;
 }
